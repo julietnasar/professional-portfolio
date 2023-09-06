83f40ee3867f364534b8585d978dbdd1203ce67f
Juliet Nasar <73414329+julietnasar@users.noreply.github.com>
2023-09-06 03:35:46 +0000

    first day build! started with an intro section

---
+++ my-app/src/Components/Intro.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <div class="intro">
+        <br/>
+        <h1 class="greeting">Hi! I'm Juliet</h1>
+        <div class="center-col">
+            <h4>a passionate and creative problem solver looking to transition into a career in Software Engineering</h4>
+            <h4>Learn More About me Below!</h4>
+            <div class="nav">
+                <a>Education</a>
+                <br/>
+                <a>Experience</a>
+                <br/>
+                <a>Key Projects</a>
+                <br/>
+                <a>Skills</a>
+            </div>
+        </div>
+        <br/>
+    </div>
+  );
+};
+
+export default Intro;
